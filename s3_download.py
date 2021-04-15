9fb0f724894b5306d5464eba864b13d461660d27
Charan Chintha <chinthacharan2@gmail.com>
2021-04-15 11:34:37 +0530

    Update s3_download.py

--- s3_download.py
+++ s3_download.py
@@ -4,6 +4,6 @@ session = boto3.Session(profile_name='abc_user')
 #print(session.available_profiles)
 client = session.client('s3')
 filename=input('Please enter the filename: ')
-bucket_name='hrc-db-dumps'
+bucket_name='my_bucket'
 download = client.download_file(bucket_name,filename,filename)
 print('Download successfully')
