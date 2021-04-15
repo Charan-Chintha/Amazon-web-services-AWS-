2690420ad1771fd4c075ce7d62ae065e05a8f02e
Charan Chintha <chinthacharan2@gmail.com>
2021-04-15 11:35:11 +0530

    Update s3_upload.py

--- s3_upload.py
+++ s3_upload.py
@@ -4,7 +4,6 @@ session = boto3.Session(profile_name='abc_user')
 #print(session.available_profiles)
 client = session.client('s3')
 filename=input('Please enter filepath:')
-#bucket_name='hrc-db-dumps/screwdriver'
-bucket_name='hrc-db-dumps'
+bucket_name='my_dumps'
 upload = client.upload_file(filename,bucket_name,filename)
 print('Upload successfully')
